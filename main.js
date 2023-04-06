0e21509b4b7d0b768341fcdc5cb8f3cb37fe155f
DhruvNandan123 <91406270+DhruvNandan123@users.noreply.github.com>
2023-04-06 13:08:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function setup(){
     canvas = createCanvas(530,490);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+Peter_pan_song="";
+Harry_potter_theme_song="";
+
+function preload(){
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
 }
